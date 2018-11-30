711b4048daa9c5a7023d261e49100e3c61d12a5a
ggboss <992967085@qq.com>
2018-11-30 23:11:53 +0800

    commit test1

--- src/main/java/com/zl/springboot1/controller/Test1.java
+++ src/main/java/com/zl/springboot1/controller/Test1.java
@@ -1,4 +1,10 @@
 package com.zl.springboot1.controller;
 
 public class Test1 {
+    public String test(){
+        return "success";
+    }
+    public String test1(){
+        return "error";
+    }
 }
